3ab3c08eacd35a32e8b6e14f52520259017c6da6
Liam Kirsh <liam.kirsh@riseup.net>
2016-01-24 05:06:43 -0800

    added message support

--- NLP.py
+++ NLP.py
@@ -1,7 +1,9 @@
 import re, nltk, calendar
+import sys
+import json
 from datetime import datetime, date, time, timedelta
 
-DEBUG = False
+DEBUG = False 
 
 # flags=re.IGNORECASE
 remind_patterns = [r'\b(remind|tell|send)\s+me', # "Remind me"
@@ -19,12 +21,15 @@ date_patterns = [r'\b(?:(Jan)\.?(?:uary)?|(Feb)\.?(?:ruary)?|(Mar)\.?(?:ch)?|(Ap
 # Note placement of 'midnight' before 'night' to avoid conflicts
 gen_times = [('tonight', 21), ('morning', 9), ('noon', 12), ('afternoon', 15), ('evening', 18), ('midnight', 0), ('night', 21)]
 
-spec_times = [r'\bat\s(half past\s|a quarter past\s)?(\d)+(?::(\d+))?\s?(am|pm)?', r'\b(half past\s|a quarter past\s)?(\d)+(?::(\d+))?\s?(am|pm)?'] # try without the 'at' if it's not found initially
+spec_times = [r'\bat\s(half past\s|a quarter past\s)?(\d)+(?::(\d+))?\s?(am|pm)?']
+			#r'\b(half past\s|a quarter past\s)?(\d)+(?::(\d+))?\s?(am|pm)?'] # try without the 'at' if it's not found initially
 
-period = r'in an? (minute|hour)'
+period = r'in (an?|\d+)? (minutes?|hours?)'
  
 days = {'sunday':6, 'monday':0, 'tuesday':1, 'wednesday':2, 'thursday':3, 'friday':4, 'saturday':5}
 months = {'jan':1, 'feb':2, 'mar':3, 'apr':4, 'may':5, 'jun':6, 'jul':7, 'aug':8, 'sep':9, 'oct':10, 'nov':11, 'dec':12}
+
+message_pattern = r'"([^"]+)"'
 
 # Solution by phihag <http://stackoverflow.com/users/35070/phihag>. Used under a CC-By-SA 3.0 license.
 # http://stackoverflow.com/a/6558571
@@ -38,7 +43,7 @@ def next_weekday(d, weekday):
 # http://stackoverflow.com/a/4131114
 def add_months(sourcedate, months):
 	month = sourcedate.month - 1 + months
-	year = int(sourcedate.year + month / 12 )
+	year = int(sourcedate.year + month / 12)
 	month = month % 12 + 1
 	day = min(sourcedate.day, calendar.monthrange(year, month)[1])
 	return date(year, month, day)
@@ -62,7 +67,7 @@ def parseQuery(query):
 	#q = re.sub(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b', 'Userxx', q) # Replace email address with 'User"
 	#parsed = nltk.word_tokenize(q)
 	#parts = nltk.pos_tag(parsed)
-
+	email = ""
 	q = query.split("/\r?\n/")[0]
 	# First check for reminder format
 	qType = -1 # 0 for reminder, 1 for sendlater
@@ -153,25 +158,35 @@ def parseQuery(query):
 			if DEBUG:
 				print 'matched!'
 			now = datetime.now()
-			if match.group(1) == "minute":
-				t = (now + timedelta(minutes = 1)).time()
-			elif match.group(1) == "hour":
-				t = (now + timedelta(hours = 1)).time()
+			if match.group(1) == "a" or match.group(1) == "an":
+				quantity = 1
+			else:
+				quantity = int(match.group(1))
+				print 'quantity:', quantity
+			if match.group(2).startswith("minute"):
+				t = (now + timedelta(minutes = quantity)).time()
+			elif match.group(2).startswith("hour"):
+				t = (now + timedelta(hours = quantity)).time()
 				
 			if t.hour == 0: # it's the dawn of a new day
 				d += timedelta(1)
 		
 	
-	print "Date:", str(d)
-	print "Time:", str(t)
+	#print "Date:", str(d)
+	#print "Time:", str(t)
+	message = re.search(message_pattern, q).group(1)
 	
 	if qType == 0:
-		print 'reminder'
+		theType = 'reminder'
 	elif qType == 1 and email:
-		print 'sendlater'
-		print 'email: %s' % email
+		theType == 'sendlater'
+		#print 'email: %s' % email
 	else:
 		print 'err'
+	result = {"date": str(d), "time": str(t), "type": theType, "email": email, "message": message}
+	print json.dumps(result)
+	sys.stdout.flush
+	sys.exit(0)
 	
 	# First, determine whether it is a reminder or a sendlater
 	#for pattern in remind_patterns:

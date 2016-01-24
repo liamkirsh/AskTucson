b8181801c60dd2fe08d368a58910de9a8f5a2eb5
Liam Kirsh <liam.kirsh@riseup.net>
2016-01-23 16:00:14 -0800

    handling strange input cases

--- NLP.py
+++ NLP.py
@@ -7,12 +7,36 @@ sendlater_patterns = [r'\b(remind|tell|email|send)\s+[A-Za-z0-9._%+-]+@[A-Za-z0-
 						r'\b(?:email|send)\b(?:(?!to).)*to\s+\b([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,})'] # "Send a message to x@y.z"
 
 def parseQuery(q):
-	email = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b', q)
-	q = re.sub(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b', 'Userxx', q) # Replace email address with 'User"
+	#email = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b', q)
+	#q = re.sub(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b', 'Userxx', q) # Replace email address with 'User"
 	parsed = nltk.word_tokenize(q)
 	parts = nltk.pos_tag(parsed)
+
+	# First check for reminder format
+	qType = -1 # 0 for reminder, 1 for sendlater
+	minNdx = float('inf')
+	for pat in remind_patterns:
+		match = re.search(pat, q)
+		if match and match.start() < minNdx:
+			qType = 0
+			minRemindNdx = match.start()			
+
+	for pat in sendlater_patterns:
+		match = re.search(pat, q)
+		if match and match.start() < minNdx:
+			qType = 1
+			minRemindNdx = match.start()
+
+	if qType == 0:
+		print 'reminder'
+	elif qType == 1:
+		print 'sendlater'
+	else:
+		print 'err'
 	
 	# First, determine whether it is a reminder or a sendlater
 	#for pattern in remind_patterns:
-		
-query = user_input()
+
+while True:		
+	query = raw_input()
+	parseQuery(query)
